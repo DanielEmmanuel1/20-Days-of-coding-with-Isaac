a10fdd75da272bab685aed396d99ae4bd9f56fcd
Deon <oluwadamilare_daniel@outlook.com>
2024-04-29 23:29:01 +0100

    Fuck u Isaac

--- Day 1/practice.js
+++ Day 1/practice.js
@@ -1,102 +1,115 @@
-//Example1
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-// let count = 20;
-
-// for(i=0; i<count; count--){
-//     console.log(count)
+// function padRow(rowNumber, rowCount) {
+//   return character.repeat(rowNumber);
 // }
 
-// Example 2
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(padRow(i + 1, count));
+// }
 
-// let cityNames = ['Lagos', 'Port-Harcort', 'Enugu', 'Festac'];
-// console.log(cityNames);
+// let result = "";
 
-// cityNames.push('Ikorodu', 'ojuelegba', 'mowe')
-// console.log(cityNames);
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-// cityNames.pop();
-// console.log(cityNames)
+// console.log(result);
+// console.log(rowNumber)
 
-// let stringName = "";
+// let count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (const num of count){
+//     console.log(num)
+// }
+
+// let count = 10
+// for (i = 0; i < count; i++) {
+//     console.log(i)
+// }
+
+// let count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// count.push(11, 12, 13, 14, 15);
+
+// console.log(count);
+
+// count = count.pop()
+// console.log(count)
+
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(name) {
+//   return name;
+// }
+
+// const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// myArray.push(11, 12, 13, 14, 15);
+// console.log(myArray)
+
+// const count = 10;
+// for (i=0; i<count; i++){
+//   console.log(i);
+// }
+
+// let a = 2;
+// let b = 8;
+
+// function addNumbers(){
+//   return a + b;
+// }
+
+// console.log(addNumbers());
+
+// let a = 5;
+
+// function myLetter() {
+//   a = 8;
+//   console.log(a);
+// }
+
+// myLetter();
+
+// const title = "Professor ";
+// function demo(name) {
+//   let profName = title + name;
+//   return profName;
+// }
+
+// const newName = demo("Naomi");
+
+// const stringName = `My name is ${newName}`;
 // console.log(stringName);
 
-//Example 3
-// const myArray = ['apple', 'banana', 'orange', 'grape'];
-// for (const fruit of myArray) {
-//     console.log(fruit);
+// myName("Deon");
+
+// const newName = myName("Deon");
+
+// console.log(newName)
+
+// function padRow(name) {
+//   const test = "Testing";
+//   return test;
 // }
+// const pRow = padRow()
+// console.log(pRow)
+// console.log(padRow())
+// const call = padRow("CamperChan");
+// console.log(call);
 
-// const states = ['Lagos', 'Enugu', 'Jos', 'Uyo'];
+// let greetings = "Hello there";
 
-// for(const places of states) {
-//     console.log(places)
-// }
+// greetings = greetings.repeat(3);
+// console.log(greetings)
 
-// let age = 10;
+const title = "Professor";
 
-// age = age + 10;
-
-// console.log(age)
-
-// const count = 8;
-// const rows = [];
-
-// for (let i = 0; i < count; i = i + 1) {
-//   rows.push(i);
-// }
-
-// let result = "";
-// for (const row of rows) {
-//   result = result + row;
-//   console.log(result)
-// }
-
-// let praize = "Deon is a genius";
-// praize = praize.repeat(4);
-// console.log(praize)
-
-// for (const names of praize){
-//     console.log(names)
-// }
-
-//Example 4
-// const character = "#";
-// const count = 8;
-// const rows = [];
-
-// for (let i = 0; i < count; i = i + 1) {
-//   rows.push(character);
-// }
-
-// let result = "";
-
-// for (const row of rows) {
-//   result = result + "\n" + row;
-// }
-
-// console.log(result);
-
-//Example 5
-// const character = "#";
-// const count = 8;
-// const rows = [];
-// for (let i = 0; i < count; i = i + 1) {
-//   rows.push(character.repeat(i));
-// }
-
-// let result = "";
-
-// for (const row of rows) {
-//   result = result + "\n" + row;
-// }
-
-// console.log(result);
-
-//Example 6
-function padRow() {
-    return "I am King";
+function myName(name) {
+  const test = "Daniel";
+  return title + " " + test;
 }
 
-const call = padRow();
-
-console.log(call);
+console.log(myName());
