2cb1af5b3ed749d5b599e85f06a77538b5b6bde0
Deon <oluwadamilare_daniel@outlook.com>
2024-04-30 02:09:00 +0100

    Day1 & Day 2 codes

--- Day 1/practice.js
+++ Day 1/practice.js
@@ -1,115 +1,80 @@
-// const character = "#";
-// const count = 8;
-// const rows = [];
+// ADDITION ASSIGNMENT
+// EXAMPLE 1
+let x = 10
+x += 20
+console.log(x)
 
-// function padRow(rowNumber, rowCount) {
-//   return character.repeat(rowNumber);
-// }
+// EXAMPLE 2
+let z = 20
+z += 80
+console.log(z)
 
-// for (let i = 0; i < count; i = i + 1) {
-//   rows.push(padRow(i + 1, count));
-// }
 
-// let result = "";
-
-// for (const row of rows) {
-//   result = result + "\n" + row;
-// }
-
-// console.log(result);
-// console.log(rowNumber)
-
-// let count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// for (const num of count){
-//     console.log(num)
-// }
-
-// let count = 10
-// for (i = 0; i < count; i++) {
-//     console.log(i)
-// }
-
-// let count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// count.push(11, 12, 13, 14, 15);
-
-// console.log(count);
-
-// count = count.pop()
-// console.log(count)
-
-// const character = "#";
-// const count = 8;
-// const rows = [];
-
-// function padRow(name) {
-//   return name;
-// }
-
-// const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// myArray.push(11, 12, 13, 14, 15);
-// console.log(myArray)
-
-// const count = 10;
-// for (i=0; i<count; i++){
-//   console.log(i);
-// }
-
-// let a = 2;
-// let b = 8;
-
-// function addNumbers(){
-//   return a + b;
-// }
-
-// console.log(addNumbers());
-
-// let a = 5;
-
-// function myLetter() {
-//   a = 8;
-//   console.log(a);
-// }
-
-// myLetter();
-
-// const title = "Professor ";
-// function demo(name) {
-//   let profName = title + name;
-//   return profName;
-// }
-
-// const newName = demo("Naomi");
-
-// const stringName = `My name is ${newName}`;
-// console.log(stringName);
-
-// myName("Deon");
-
-// const newName = myName("Deon");
-
-// console.log(newName)
-
-// function padRow(name) {
-//   const test = "Testing";
-//   return test;
-// }
-// const pRow = padRow()
-// console.log(pRow)
-// console.log(padRow())
-// const call = padRow("CamperChan");
-// console.log(call);
-
-// let greetings = "Hello there";
-
-// greetings = greetings.repeat(3);
-// console.log(greetings)
-
-const title = "Professor";
-
-function myName(name) {
-  const test = "Daniel";
-  return title + " " + test;
+// IF AND ELSE STATEMENTS
+let x = 9
+if (x > 10){
+  console.log('X is greater than 10')
+}else{
+  console.log('X is lesser than 10')
 }
 
-console.log(myName());
+// SWITCH STATEMENTS
+// EXAMPLE 1
+let footballerName = "Suarez";
+
+switch (footballerName) {
+  case "Ronaldo":
+    console.log("Correct, this is the player");
+    break;
+
+  case "Ballotelli":
+    console.log("This is Ballotelli");
+    break;
+
+  case "Neymar Jnr":
+    console.log("I'm Neyar ffrom Brazil");
+    break;
+
+  case "MacAllister":
+    console.log("I'm liverpool's best player currently");
+
+  default:
+    console.log("Not a players");
+}
+// EXAMPLE 2
+let fruits = "Apples";
+
+switch (fruits) {
+  case "Mangos":
+    console.log("Mangoes are yellow");
+    break;
+
+  case "Oranges":
+    console.log("Oranges are orange");
+    break;
+
+  case "Strawberry":
+    console.log("Strawberries are red");
+
+  default:
+    console.log("Its just not a fruit");
+}
+
+// FUNCTIONS
+// EXAMPLE 1
+let title = "Mr";
+let name = "Deon"
+
+function myName(title, name){
+  return title + " " + name
+}
+console.log(myName("Mr", "Deon"))
+
+
+let title = "Mr";
+let name = "Deon";
+
+function myName(){
+  return title + " " + name
+}
+console.log(myName())
