0ea740b5753c46b111a197e5c0a058b534e240d4
Deon <oluwadamilare_daniel@outlook.com>
2024-05-01 06:59:22 +0100

    Day 3 code

---
+++ Day 3/Arrays.js
@@ -0,0 +1,39 @@
+// let myArray = [1, 2, 3, 4, 5];
+// myArray.shift()
+// console.log(myArray)
+
+// let myArray = [1, 2, 3, 4, 5];
+// myArray.unshift(-1, 0)
+// console.log(myArray)
+
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// myArray.slice(0, 4)
+// console.log(myArray)
+
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// myArray = myArray.slice(0, 4);
+// console.log(myArray); // Output: [1, 2, 3, 4]
+
+// let myNewArray = [0, 1, 2, 3, 4, 5]
+// myNewArray.push(6, 7, 8, 9, 10);
+// console.log(myNewArray)
+
+// myNewArray.pop()
+// console.log(myNewArray)
+
+// myNewArray.shift()
+// console.log(myNewArray)
+
+// myNewArray.unshift(0)
+// console.log(myNewArray)
+
+
+// let myNewArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// myNewArray.splice(1, 4)
+// console.log(myNewArray)
+
+// let ageCount = 20;
+
+// for(i = 12; i < ageCount; i++){
+//     console.log(i)
+// }
---
+++ Day 3/Loops.js
@@ -0,0 +1,12 @@
+// let ageCount = 20;
+
+// for(i = 12; i < ageCount; i++){
+//     console.log(i)
+// }
+
+// let ageCount = 10;
+// let myAge = 30;
+
+// while(myAge > ageCount){
+//     console.log(ageCount++)
+// }
