391a90be70e6ca50c7b834d404ad772d066cab5a
Deon <oluwadamilare_daniel@outlook.com>
2024-05-01 07:58:05 +0100

    Day 4

---
+++ Day 4/jsobjects.js
@@ -0,0 +1,26 @@
+// let Person = {
+//     name: "Deon",
+//     age: 24,
+//     Bsc: "Computer Science",
+//     salaryYearly: "$80000" 
+// }
+
+// Person['city'] = "Lagos"
+// Person.age = 10
+
+// console.log(Person)
+
+
+
+// let Person = {
+//     name: "Deon", 
+//     age: 24,
+//     stateOfOrigin: {
+//         city: "Ikot Abasi",
+//         LGA: "Ete"
+//     }, 
+//     currentAddress: "Alade Street, Shomolu"
+// };
+
+
+// console.log(Person['stateOfOrigin'])
