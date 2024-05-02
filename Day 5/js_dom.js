ff173cf9594634cd13288b1065410ebdab2b9d06
Deon <oluwadamilare_daniel@outlook.com>
2024-05-02 23:43:42 +0100

    Day 5

---
+++ Day 5/js_dom.js
@@ -0,0 +1,13 @@
+// let idContainer = document.getElementById('container')
+// console.log('container')
+
+//  var paragraph = document.createElement("p");
+//  paragraph.textContent = "This is a new paragraph.";
+
+//  var container = document.getElementById("container");
+//  container.appendChild(paragraph);
+
+// let button = document.getElementById("myButton");
+// button.addEventListener("click", function () {
+//   alert("Button clicked!");
+// });
