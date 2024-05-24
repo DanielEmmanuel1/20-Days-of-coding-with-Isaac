313cd230147a9150f437cadec1d6bc7a365d3761
Deon <oluwadamilare_daniel@outlook.com>
2024-05-24 03:58:52 +0100

    Just me relearning

--- Day 15/app.js
+++ Day 15/app.js
@@ -29,7 +29,58 @@
 // let mySisName = "debby"
 // console.log(mySisName.startsWith("d"))
 
-let userName = "olwadamilare"
-if(userName.startsWith("")){
+// let userName = "olwadamilare"
+// if(userName.startsWith("")){
     
-}
+// }
+
+// let temp = 0
+
+// if(temp == 100){
+//     console.log("Boiling point reached")
+// } else if(temp > 100){
+//     console.log("exceeded boiling point")
+// } else if(temp <= 0){
+//     console.log("Freezing point reached")
+// }else{
+//     console.log("Normal temp")
+// }
+
+// let temp = 61;
+
+// if (temp > 30 && temp <= 60) {
+//     console.log("The weather is great");
+// }else if(temp > 60) {
+//     console.log("weather is too hot")
+// }else if(temp <= 30 && temp !== 0){
+//     console.log("Weather is cold")
+// }else if(temp <= 0){
+//     console.log("Weather is freezing")
+// }else{
+//     console.log("I dont know")
+// }
+
+
+// let isSunny = true
+
+// if(isSunny){
+//     console.log("Its a sunny day")
+// }else{
+//     console.log("Its cloudy")
+// }
+
+// let PI = 3.142
+
+// if(PI !== "3.142") {
+//     console.log("This NOT is PI")
+// } else {
+//     console.log("This is PI")
+// }
+
+// let userName = ""
+
+// while(userName === ""){
+//     userName = prompt("Enter Username")
+// }
+
+// console.log(`Welcome ${userName}`);
